cc8aff5342c1533514989f2bddcd165b256d42af
Drapliks <kinstevg@gmail.com>
2026-01-31 13:07:45 +0500

    Update PyManeg functional

--- PyManegTerminal.py
+++ PyManegTerminal.py
@@ -6,7 +6,7 @@ os.system('mkdir ~/PythonProjects')
 
 os.system('clear')
 
-print('Welcome to PyManeg!\n1.Open file\n2.New file\n3.Launch File\n4.Rename file\n5.Delete file\n0.Exit')
+print('Welcome to PyManeg!\n1.Open file\n2.New file\n3.Open project\n4.New project\n5.Launch File\n6.Rename file\n7.Delete file\n0.Exit')
 
 while True:
     act = input('>')
@@ -23,18 +23,43 @@ while True:
         newfilename = input('Enter file name: ')
         os.system('touch ~/PythonFiles/' + newfilename + '.py')
         print('Successfully!')
-       
+    
     if act == '3':
+        endless = True
+        openprojectname = input('Enter project name: ')
+        print('Project: ' + openprojectname + '\n1.Open main.py\n2.Open file\n3.New file\n0.Exit')
+        if endless == True:
+            actinopenproject = input('>>')
+            if actinopenproject == "1":
+                os.system('nvim ~/PythonProjects/' + openprojectname +  'main.py')
+                actinopenproject = input('>>')
+            if actinopenproject == "2":
+                openfileipname = input('Enter file name: ')
+                os.system('nvim ~/PythonProjects/' + openprojectname + "/" + openfileipname + '.py')
+                actinopenproject = input('>>')
+            if actinopenproject == "3":
+                newfileipname = input('Enter file name: ')
+                os.system('touch ~/PythonProjects/' + openprojectname + '/' + newfileipname + '.py')
+            if actinopenproject == "0":
+                endless = False
+
+    if act == '4':
+        newprojectname = input('Enter project name: ')
+        os.system('mkdir ~/PythonProjects/' + newprojectname)
+        os.system('python -m venv ~/PythonProjects/' + newprojectname +'/venv')
+        os.system('touch ~/PythonProjects/' + newprojectname + '/main.py')
+        print('Successfully!')
+    if act == '5':
         laucnhnamefile = input('Enter file name: ')
         os.system('python ' + laucnhnamefile + '.py')
     
-    if act == '4':
+    if act == '6':
         oldnameoffile = input('Old name: ')
         newnameoffile = input('New name: ')
         os.system('mv ~/PythonFiles/' + oldnameoffile + '.py' + ' ' + newnameoffile + '.py')
         print('Successfully!')
     
-    if act == '5':
+    if act == '7':
         deletefilename = input('Enter file name: ')
         really = input('Are you sure?(y/n)')
         
